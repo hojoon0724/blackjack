4d4d98ceb0bf16e75819d855ccc11d92713a5b5d
Hojoon Kim <hojoon0724@gmail.com>
2023-11-26 21:07:44 -0800

    shuffle and show added

--- 01-show-card/showcard.js
+++ 01-show-card/showcard.js
@@ -1,7 +1,8 @@
 const suits = ["01", "02", "03", "04"];
 const cardNums = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11J", "12Q", "13K"];
 
-const cardDeck = [];
+let cardDeck = [];
+const orderedCardDeck = [];
 
 let cardObjectTemplate = {
   cardName: "",
@@ -15,12 +16,13 @@ function makeCardArray() {
         cardName: `${s}-${n}`,
         cardSvg: `../SVGs/${s}-${n}.svg`,
       };
-      cardDeck.push(cardObjectTemplate);
+      orderedCardDeck.push(cardObjectTemplate);
     });
   });
 }
 
 makeCardArray();
+cardDeck = orderedCardDeck;
 
 // Show any random card, with repeats
 const nextButton = document.querySelector("#next");
@@ -29,3 +31,53 @@ nextButton.addEventListener("click", () => {
   const currentCard = document.querySelector("#current-show");
   currentCard.innerHTML = `<img src="${cardDeck[randomIndex].cardSvg}">`;
 });
+
+let shuffledDeck = [];
+function shuffle() {
+  shuffledDeck = [];
+  console.log("beforeCRD", cardDeck);
+  console.log("beforeORD", orderedCardDeck);
+  console.log("beforeSHF", shuffledDeck);
+
+  for (i = cardDeck.length - 1; i >= 0; i--) {
+    let grabIndex = Math.floor(Math.random() * i);
+    // console.log(cardDeck[grabIndex]);
+    shuffledDeck.push(cardDeck[grabIndex]);
+    cardDeck.splice(grabIndex, 1);
+  }
+
+  console.log("shuffle done");
+  console.log("afterCRD", cardDeck);
+  console.log("afterORD", orderedCardDeck);
+  console.log("afterSHF", shuffledDeck);
+}
+
+shuffle();
+
+function showDeck() {
+  const fullDeckDiv = document.querySelector("#full-deck");
+  for (i = 0; i < shuffledDeck.length; i++) {
+    let card = document.createElement("img");
+    // console.log(card, i);
+    card.setAttribute("src", `${shuffledDeck[i].cardSvg}`);
+    fullDeckDiv.appendChild(card);
+    console.log("show deck done");
+  }
+}
+
+function clearCards() {
+  const fullDeckDiv = document.querySelector("#full-deck");
+  fullDeckDiv.innerHTML = "";
+}
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  makeCardArray();
+  clearCards();
+  shuffle();
+  showDeck();
+});
+
+// showDeck();
+
+// console.log(shuffledDeck);
