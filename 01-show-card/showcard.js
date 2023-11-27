e159e704e7ac2161a419e97c701af732bad3f219
Hojoon Kim <hojoon0724@gmail.com>
2023-11-26 23:29:44 -0800

    multiple decks now possible

--- 01-show-card/showcard.js
+++ 01-show-card/showcard.js
@@ -5,30 +5,36 @@ let cardDeck = [];
 
 let cardObjectTemplate = {
   cardName: "",
+  // 2-6 = +1
+  // 7-9 = 0
+  // 10-A = -1
+  cardValue: "",
   cardSvg: "",
 };
 
-function makeCardArray() {
+function makeCardArray(deckCount) {
   suits.forEach((s) => {
-    cardNums.forEach((n) => {
+    cardNums.forEach((n, i) => {
+      if (i > 8 || i === 0) {
+        v = -1;
+      } else if (i < 5) {
+        v = 1;
+      } else {
+        v = 0;
+      }
       cardObjectTemplate = {
         cardName: `${s}-${n}`,
+        cardValue: v,
         cardSvg: `../SVGs/${s}-${n}.svg`,
       };
-      cardDeck.push(cardObjectTemplate);
+      for (repetition = deckCount; repetition > 0; repetition--) {
+        cardDeck.push(cardObjectTemplate);
+      }
     });
   });
 }
 
-makeCardArray();
-
-let cardArrayIndexNum = -1;
-const currentCard = document.querySelector("#current-show");
-const nextButton = document.querySelector("#next");
-nextButton.addEventListener("click", () => {
-  cardArrayIndexNum++;
-  currentCard.innerHTML = `<img src="${cardDeck[cardArrayIndexNum].cardSvg}">`;
-});
+makeCardArray(0);
 
 let shuffledDeck = [];
 function shuffle(deck) {
@@ -55,13 +61,48 @@ function clearCards() {
   fullDeckDiv.innerHTML = "";
 }
 
+const runCount = document.querySelector("#run");
+function showRunningCount() {
+  runCount.innerText = cardArrayIndexNum + 1;
+}
+
+function showTheCount() {
+  const theCountDiv = document.querySelector("#the-count");
+  theCountDiv.innerText = theCount;
+}
+
+function getDeckAmount() {
+  const deckInput = document.querySelector("#deck-amount");
+  return deckInput.value;
+}
+
 const reset = document.querySelector("#reset");
 reset.addEventListener("click", () => {
+  cardDeck = [];
+  makeCardArray(getDeckAmount());
   cardArrayIndexNum = -1;
   currentCard.innerHTML = "";
   clearCards();
   shuffle(cardDeck);
   showDeck();
+  runCount.innerText = 0;
+  theCount = 0;
 });
 
 showDeck();
+
+let theCount = 0;
+function addUpTheCount() {
+  theCount += cardDeck[cardArrayIndexNum].cardValue;
+}
+
+let cardArrayIndexNum = -1;
+const currentCard = document.querySelector("#current-show");
+const nextButton = document.querySelector("#next");
+nextButton.addEventListener("click", () => {
+  cardArrayIndexNum++;
+  currentCard.innerHTML = `<img src="${cardDeck[cardArrayIndexNum].cardSvg}">`;
+  addUpTheCount();
+  showRunningCount();
+  showTheCount();
+});
